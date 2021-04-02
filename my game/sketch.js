0730a46841632e0386c21323343b27bcfdf0d33e
dimitri-lagios <73685688+dimitri-lagios@users.noreply.github.com>
2021-04-02 21:18:03 +1100

    Add files via upload

--- my game/sketch.js
+++ my game/sketch.js
@@ -2,38 +2,80 @@
 var platform
 var player
 var jump =0
-var spike
-var spikeWall1
-var spikeWall2
-var bat1
-var bat2
+var spike, spikeWall1, spikeWall2
+var bat1, bat2
 var batFly
 var brickAB = 0
+var gameState = 1
+var brickImg
+var brickPFImg, brickPFImg2
+var doorImg
+var wallImg1, wallImg2, roofImg, floorImg
+var lavaImg1, lavaImg2
 
 function preload(){
- batFly = loadAnimation('images/bat1.png','images/bat2.png','images/bat3.png','images/bat4.png');
+  batFly = loadAnimation('images/bat1.png','images/bat2.png','images/bat3.png','images/bat4.png');
 
-spikeImg1 = loadImage('images/spike.png');
-spikeImg2 = loadImage('images/spikeSide.png');
-spikeImg3 = loadImage('images/spikeSide.png');
+  spikeImg1 = loadImage('images/spike.png');
+  spikeImg2 = loadImage('images/spikeSide.png');
+  spikeImg3 = loadImage('images/spikeSide.png');
+
+  brickImg = loadImage('images/brick-item.png')
+  playerImg = loadImage('images/player.png')
+
+  brickPFImg = loadImage('images/brick-platform.png')
+  brickPFImg2 = loadImage('images/brick-platform.png')
+
+  wallImg1 = loadImage('images/brick-platform1.png')
+  wallImg2 = loadImage('images/brick-platform1.png')
+  roofImg = loadImage('images/brick-platform1.png')
+  floorImg = loadImage('images/brick-platform1.png')
+
+  doorImg = loadImage('images/door.jpg')
+
+  lavaImg1 = loadImage('images/lava.gif')
+  lavaImg2 = loadImage('images/lava.gif')
 
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  platform1 = createSprite(150, height-120, 50, 20);
-  platform2 = createSprite(width/4+20, height-200, 50, 20);
-  platform3 = createSprite(width/2, height-280, 70, 20);
-  platform4 = createSprite(3*width/4, height-360, 50, 20);
-  platform5 = createSprite(width -150, height-440, 50, 20);
-
-  player = createSprite(200, 200, 10, 20);
   
+  platform1 = createSprite(150, height-120, 50, 22);
+  platform2 = createSprite(width/4+20, height-200, 50, 22);
+  platform3 = createSprite(width/2, height-280, 70, 22);
+  platform4 = createSprite(3*width/4, height-360, 50, 22);
+  platform5 = createSprite(width -150, height-440, 50, 22);
+
+  platform1.addImage(brickPFImg);
+  platform2.addImage(brickPFImg);
+  platform3.addImage(brickPFImg2);
+  platform4.addImage(brickPFImg);
+  platform5.addImage(brickPFImg);
+
+  brickPFImg.resize(50,22)
+  brickPFImg2.resize(70,22)
+
+
+  player = createSprite(150, height-100, 10, 20);
+  player.addImage(playerImg);
+  //player.resize()
+  player.debug=true
+
   wall1 = createSprite(50, height/2, 20, height-100);
   wall2 = createSprite(width-50, height/2, 20, height-100);
   roof = createSprite(width/2, 50, width-80, 20);
   floor = createSprite(width/2, height-50, width-80, 20);
   
+  wall1.addImage(wallImg1)
+  wall2.addImage(wallImg2)
+  roof.addImage(roofImg)
+  floor.addImage(floorImg)
+
+  wallImg1.resize(20, height-100)
+  wallImg2.resize(20, height-100)
+  roofImg.resize(width-80, 20)
+  floorImg.resize(width-80, 20)
 
   bat1 = createSprite(3*width/8, height-280, 20, 20);
   bat2 = createSprite(5*width/8,height-280,20,20);
@@ -58,8 +100,12 @@ function setup() {
   invisWall4.visible = false
 
   spike = createSprite(width/2, height-300,10,20);
-  spikeWall1 = createSprite(width/4+20-25, height-200,20,10);
-  spikeWall2 = createSprite(3*width/4-25, height-360,20,10);
+  spikeWall1 = createSprite(width/4-15, height-200,20,10);
+  spikeWall2 = createSprite(3*width/4-35, height-360,20,10);
+
+  
+//spikeWall1.debug=true
+//spikeWall2.debug=true
 
   spike.addImage(spikeImg1);
   spikeWall1.addImage(spikeImg2);
@@ -70,47 +116,53 @@ function setup() {
   spikeWall2.scale = 0.3
 
   door = createSprite(width-55, height/2,20,30);
+  door.addImage(doorImg)
+  doorImg.resize(20,30)
 
-  lavaTop = createSprite(width-55, height/2-30,40,10)
-  lavaSide = createSprite(width-90, height/2,10,40)
+  lavaTop = createSprite(width-80, height/2-30,45,10)
+  lavaSide = createSprite(width-98, height/2-6,10,40)
 
- // brickItem = createSprite(width -150, height-470,10,10);
+  lavaTop.addImage(lavaImg1);
+  lavaSide.addImage(lavaImg2);
+  lavaImg1.resize(45,10)
+  lavaImg2.resize(10,40)
+
+  brickItem = createSprite(width -150, height-470,10,10);
+ // brickItem = createSprite(width -150, height-100,10,10); 
+  brickItem.addImage(brickImg);
+  brickImg.resize(10,10)
   
-  brickItem = createSprite(width -150, height-100,10,10);
+ 
   
 }
 
 function draw() {
   background(70);  
-  //console.log(jump)
-  console.log(brickAB)
   
 
+
+if(gameState === 2){
+  fill(0,255,0)
+  textSize(40)
+  text("you win!!!",width/2,height/2)
+  player.setVelocity(0,0)
+}
+if(gameState===1){
+  fill(0,0,255)
+  text("use mouse to position and click to place platform",width -340, height-490)
+  
   interaction()
   batMovement()
   playerMovement()
   drawSprites();
 }
+}
 
 function playerMovement(){
-  /*if(keyDown('space')&&
-  (
-    player.collide(floor)|| 
-    player.collide(platform1)||
-    player.collide(platform2)||
-    player.collide(platform3)||
-    player.collide(platform4)||
-    player.collide(platform5)
-  )
-  ){
+  if(keyDown('space')&& jump >=1){
+    jump = jump -1 
     player.velocityY = -10
   }
-*/
-
-if(keyDown('space')&& jump >=1){
-  jump = jump -1 
-  player.velocityY = -10
-}
 
   if(jump == 0 &&
   (
@@ -119,7 +171,9 @@ if(keyDown('space')&& jump >=1){
     player.collide(platform2)||
     player.collide(platform3)||
     player.collide(platform4)||
-    player.collide(platform5)
+    player.collide(platform5) || 
+    player.collide(brickItem)
+
   )
   ){
     jump = jump +1
@@ -145,6 +199,7 @@ if(keyDown('space')&& jump >=1){
   player.collide(platform3)
   player.collide(platform4)
   player.collide(platform5)
+  player.collide(brickItem)
 }
 
 function batMovement(){
@@ -158,49 +213,29 @@ function batMovement(){
 
 function interaction(){
   if(player.isTouching(bat1) || player.isTouching(bat2) 
-  || player.isTouching(spike) || player.isTouching(spikeWall1) || player.isTouching(spikeWall2)){
-    player.x = 200
-    player.y = 200  
+  || player.isTouching(spike) || player.isTouching(spikeWall1) || player.isTouching(spikeWall2)
+  || player.isTouching(lavaSide) || player.isTouching(lavaTop)){
+    player.x = 150
+    player.y = height-100  
+ 
   }
 
   if(player.isTouching(brickItem) && brickAB !==2){
     brickAB = 1;
-  // brickItem.x = mouseX
+    brickImg.resize(20,20)
   }
 
   if(brickAB === 1){
     brickItem.x = mouseX;
     brickItem.y = mouseY;
-  }else if(brickAB === 2){
-  brickItem.collide(player)
+    if(mouseIsPressed){
+      brickAB = 2;
+    }
   }
-
-}
-
-function mousePressed(){
-if(brickAB===1);
-brickAB = 2;
+  
+if(player.isTouching(door)){
+  gameState = 2
 }
 
   
-
-
-
-/*
-
-if(brickAB === 1){
-  Brickitem.x = mouseX
-  placedBrick.y = mouseY
-}else{
-  placedBrick.collide(player)
 }
-
-}
-
-
-
-function placeBrick(){
-placedBrick = createSprite(mouseX,mouseY,20,20);
-brickAB = 1
-}
-*/
